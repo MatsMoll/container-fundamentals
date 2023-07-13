67b743165c6bf617c782d0194313ed681fc3dcfd
Mats E. Mollestad <mats@mollestad.no>
2023-07-13 17:45:47 +0200

    reduced the size of the docker image

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+docker build -f edge_server/Dockerfile -t container-fund:v1 edge_server
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+
+docker run -p 8000:80 --restart=always -d container-fund:v1 
